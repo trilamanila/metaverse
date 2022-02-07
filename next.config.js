0d54a2cac1782e7f17e50de374ca08ae7aed61f7
Joncin Picol <jctrilamanila@gmail.com>
2022-02-07 06:34:00 -0500

    removed next.config

--- next.config.js
+++
@@ -1,9 +0,0 @@
-const withImages = require('next-images')
-module.exports = withImages()
-
-module.exports = {
-  reactStrictMode: true,
-  images: {
-    domains: ['avatars.dicebear.com'],
-  },
-}
